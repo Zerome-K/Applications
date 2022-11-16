8ac1f6751f179511e6be345528cf1cfe6e4d9c60
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-11-16 17:34:59 +0530

    Add files via upload

--- com/zerome/carparkingmanagementsystem/Admin.java
+++ com/zerome/carparkingmanagementsystem/Admin.java
@@ -3,36 +3,120 @@ package carParkingManagementSystem;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Admin {
-    HashMap<String, ParkerDetail> data = new HashMap<>();
-    int totalSlots = 18, usedSlots = 0, freeSlots = 18;
-    double revenue = 0.0;
-    Floor floor;
+    private HashMap<String, ParkerDetail> data = new HashMap<>();
+
+    private int totalSlots = 18, usedSlots = 0, freeSlots = 18;
+
+    private double revenue = 0.0;
+    private Floor floor;
+
     Admin() {
         floor = new Floor();
     }
 
-    public String add() {
+    public void getStart() {
+        Scanner in = new Scanner(System.in);
+        Admin admin = new Admin();
+        System.out.println("=-=-=-=-=-=-=- ZOHO PARKING -=-=-=-=-=-=-=");
+        while (true) {
+            System.out.println(
+                    """
+
+                            1. ASSIGN SLOT
+                            2. DE-ASSIGN SLOT
+                            3. VIEW AVAILABILITY
+                            4. PARKERS RECORDS
+                            5. VIEW PARKING AREA
+                            6. EXIT""");
+
+            System.out.print("\nENTER OPTION : ");
+            int option;
+            while (true) {
+                try{ option = in.nextInt();
+                    if(option>= 1 && option <= 6)break;
+                } catch (InputMismatchException e){System.out.println("=- ENTER VALID INPUT -=");}
+            }
+            if (option == 1) {
+                String slot = admin.checkIn();
+                System.out.println("SLOT ASSIGNED SUCCESSFULLY : ID : " + slot);
+            } else if (option == 2) {
+                System.out.print("ENTER ID : ");
+                while(true) {
+                    String id = in.next();
+                    Pattern pattern = Pattern.compile("\\d{3}");
+                    Matcher match = pattern.matcher(id);
+                    if (match.find()) {admin.checkOut(id);break;}
+                    System.out.println("ENTER VALID ID : ");
+                }
+            } else if (option == 3) {
+                admin.records();
+            } else if (option == 4) {
+                admin.parkerdetails();
+            } else if (option == 5) {
+                admin.floor.platform();
+            } else break;
+        }
+    }
+
+    public String checkIn() {
 
         LocalTime time = LocalTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        ParkerDetail p = new ParkerDetail();
-
+        ParkerDetail parkerDetail = new ParkerDetail();
         String slot = floor.getSlot();
-
-        p.time = time.format(formatter);
-        p.status = "IN";
+        parkerDetail.time = time.format(formatter);
+        parkerDetail.status = "IN";
         usedSlots++;
         freeSlots--;
-
-        data.put(slot, p);
-
+        data.put(slot, parkerDetail);
         return slot;
+    }
 
+    public void checkOut(String id) {
+
+        ParkerDetail parkerDetail = data.get(id);
+        String time = parkerDetail.time;
+        parkerDetail.status = "OUT";
+        LocalTime time1 = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String currentTime = time1.format(formatter);
+        parkerDetail.uptime = currentTime;
+        double total = (double) timeCalculator(time, currentTime) / 60;
+        revenue += total;
+        usedSlots--;
+        freeSlots++;
+        bill(parkerDetail, currentTime, total);
+        floor.set(id);
+    }
+
+    static int timeCalculator(String time, String currtime) {
+
+        String[] bookingTime = time.split(":");
+        String[] leavingTime = currtime.split(":");
+        int bookingHour = Integer.parseInt(bookingTime[0]), bookingmin = Integer.parseInt(bookingTime[1]), bookingsec = Integer.parseInt(bookingTime[2]);
+        int leavingHour = Integer.parseInt(leavingTime[0]), leavingMin = Integer.parseInt(leavingTime[1]), leavingSec = Integer.parseInt(leavingTime[2]);
+        int seconds = 0;
+        while (leavingSec < bookingsec) {
+            leavingMin -= 1;
+            leavingSec += 60;
+        }
+        seconds += leavingSec - bookingsec;
+        while (leavingMin < bookingmin) {
+            leavingMin += 60;
+            leavingHour -= 1;
+        }
+        seconds += (leavingMin - bookingmin) * 60;
+        if (leavingHour <= bookingHour)
+            return seconds;
+        else
+            return (seconds + (leavingHour - bookingHour) * 60 * 60);
     }
 
     public void records() {
@@ -54,106 +138,16 @@ public class Admin {
         }
     }
 
-    public void leave(String id) {
-
-        ParkerDetail p = data.get(id);
-
-        String time = p.time;
-        p.status = "OUT";
-
-
-        LocalTime t = LocalTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        String currenttime = t.format(formatter);
-
-        p.uptime = currenttime;
-
-        double total = (double) timeCalculator(time, currenttime) / 60;
-
-        revenue += total;
-
-        usedSlots--;
-        freeSlots++;
-
-        bill(p, total, currenttime, total);
-        floor.set(id);
-    }
-
-    private void bill(ParkerDetail p, double total, String currenttime, double i) {
-        String wtime = String.format("%.2fMin's", i);
-        String fair = String.format("%.2f$", i * 10);
+    private void bill(ParkerDetail p, String currenttime, double price) {
+        String duration = String.format("%.2fMin's", price);
+        String fair = String.format("%.2f$", price * 10);
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         System.out.print("| NAME           : " + centerString(p.name) + " |" + "\n| VEHICLE NUMBER : " + centerString(p.vehicleNumber) + " |" + "\n| ENTRY TIME     : " + centerString(p.time) + " |");
-        System.out.printf("\n| EXIT TIME      : " + centerString(currenttime) + " |" + "\n| WITHSTAND TIME : " + centerString(wtime) + " |" + "\n| FAIR           : " + centerString(fair) + " |");
+        System.out.printf("\n| EXIT TIME      : " + centerString(currenttime) + " |" + "\n| WITHSTAND TIME : " + centerString(duration) + " |" + "\n| FAIR           : " + centerString(fair) + " |");
         System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
     }
 
     private static String centerString(String s) {
         return String.format("%-" + 16 + "s", String.format("%" + (s.length() + (16 - s.length()) / 2) + "s", s));
-    }
-
-    static int timeCalculator(String time, String currtime) {
-
-        String[] btime = time.split(":");
-
-        String[] ltime = currtime.split(":");
-
-        int bookinghr = Integer.parseInt(btime[0]), bookingmin = Integer.parseInt(btime[1]), bookingsec = Integer.parseInt(btime[2]);
-
-        int leavinghr = Integer.parseInt(ltime[0]), leavingmin = Integer.parseInt(ltime[1]), leavingsec = Integer.parseInt(ltime[2]);
-
-        int sec = 0;
-
-        while (leavingsec < bookingsec) {
-            leavingmin -= 1;
-            leavingsec += 60;
-        }
-
-        sec += leavingsec - bookingsec;
-        while (leavingmin < bookingmin) {
-            leavingmin += 60;
-            leavinghr -= 1;
-        }
-
-        sec += (leavingmin - bookingmin) * 60;
-        if (leavinghr <= bookinghr)
-            return sec;
-        else
-            return (sec + (leavinghr - bookinghr) * 60 * 60);
-    }
-
-    public void getStart() {
-
-        Scanner in = new Scanner(System.in);
-
-        Admin admin = new Admin();
-
-        System.out.println("=-=-=-=-=-=-=- ZOHO PARKING -=-=-=-=-=-=-=");
-        while (true) {
-            System.out.println(
-                    "\n1. ASSIGN SLOT" +
-                            "\n2. DE-ASSIGN SLOT" +
-                            "\n3. VIEW AVAILABILITY" +
-                            "\n4. PARKERS RECORDS" + "\n5. VIEW PARKING AREA" + "\n6. EXIT");
-
-            System.out.print("\nENTER OPTION : ");
-            int option = in.nextInt();
-            if (option == 1) {
-                String slot = admin.add();
-                System.out.println("SLOT ASSIGNED SUCCESSFULLY : ID : " + slot);
-            } else if (option == 2) {
-                System.out.print("ENTER ID : ");
-                String id = in.next();
-                admin.leave(id);
-            } else if (option == 3) {
-                admin.records();
-            } else if (option == 4) {
-                admin.parkerdetails();
-            } else if (option == 5) {
-                admin.floor.display();
-            } else if (option == 6) break;
-        }
     }
 }
--- com/zerome/carparkingmanagementsystem/ParkerDetail.java
+++ com/zerome/carparkingmanagementsystem/ParkerDetail.java
@@ -8,9 +8,8 @@ import java.util.regex.Pattern;
 
 public class ParkerDetail{
 
-    String time, status="-", uptime, vehicleNumber, name;
-    Scanner in  = new Scanner(System.in);
-
+    public String time, status="-", uptime, vehicleNumber, name;
+    private final Scanner scanner = new Scanner(System.in);
     static ArrayList<ParkerDetail> data = new ArrayList<>();
 
     ParkerDetail(){
@@ -24,7 +23,7 @@ public class ParkerDetail{
     private String getName() {
         System.out.print("\nENTER NAME : ");
         while(true) {
-            String name = in.nextLine();
+            String name = scanner.nextLine();
             Pattern p = Pattern.compile("^[A-Za-z]\\w{2,29}$");
             Matcher m = p.matcher(name);
             if(m.find())return name;
@@ -37,9 +36,9 @@ public class ParkerDetail{
         jh:
         while(true) {
             boolean flag = false;
-            String number = in.nextLine();
-            Pattern p = Pattern.compile("^[A-Za-z]{2}\\s?\\d{2}\\s?[A-Za-z]{2}\\s?\\d{4}$");
-            Matcher m = p.matcher(number);
+            String number = scanner.nextLine();
+            Pattern pattern = Pattern.compile("^[A-Za-z]{2}\\s?\\d{2}\\s?[A-Za-z]{2}\\s?\\d{4}$");
+            Matcher matcher = pattern.matcher(number);
             for (ParkerDetail x : ParkerDetail.data) {
                 if(Objects.equals(x.vehicleNumber, number)) {
                     System.out.println("\n=-=- NUMBER ALREADY EXISTS! -=-=");
@@ -47,13 +46,9 @@ public class ParkerDetail{
                     continue jh;
                 }
             }
-            if(m.find()) return number;
+            if(matcher.find()) return number;
             System.out.println("\nENTER VALID NUMBER : ");
         }
-    }
-
-    private static String centerString(String s) {
-        return String.format("%-" + 14 + "s", String.format("%" + (s.length() + (14 - s.length()) / 2) + "s", s));
     }
 
     public String toString(){
@@ -65,5 +60,8 @@ public class ParkerDetail{
         System.out.println("| STATUS   : " + centerString(this.status)+ "|");
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         return "";
+    }
+    private static String centerString(String s) {
+        return String.format("%-" + 14 + "s", String.format("%" + (s.length() + (14 - s.length()) / 2) + "s", s));
     }
 }
